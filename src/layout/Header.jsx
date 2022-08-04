32e1ba5bb739452a8ac3a04162bb6e96bb05ac8e
Meshhur <meshhuratageldiyev010@gmail.com>
2022-08-04 10:01:04 +0300

    commited new files

---
+++ src/layout/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import arka_logo from "../images/png/arka-logo.png";
+import subaru_stars from "../images/png/subaru.png";
+import cart from "../images/png/cart.png";
+
+function Header() {
+    return (
+        <header className='header'>
+            <img className='logo' src={arka_logo} alt="our logo" />
+            <nav className='nav'>
+                <ul className='nav-ul'>
+                    <li>
+                        <img className='li-star' src={subaru_stars} alt="stars of subaru motors" />
+                        <a href="">Start your design</a>
+                    </li>
+                    <li>
+                        <a href="">All products</a>
+                    </li>
+                    <li>
+                        <a href="">Inspiration</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                </ul>
+            </nav>
+            <div className="profile">
+                <a href="">ACCOUNT</a>
+                <img src={cart} alt="cart" />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
