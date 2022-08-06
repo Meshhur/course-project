99b22775052ec1e162e203bc626a1535e740e6e9
Meshhur <meshhuratageldiyev010@gmail.com>
2022-08-06 10:37:29 +0300

    modified buttons

--- src/layout/buttons/Buttons.jsx
+++ src/layout/buttons/Buttons.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import MyButton from '../../components/UI/button/MyButton'
+import React from 'react';
+import MyButton from '../../components/UI/button/MyButton';
+import "./Buttons.css";
 
 function Buttons() {
-  return (
-    <article className='buttons'>
-        <h2 className="getToIt">Let`s get to it</h2>
-        <MyButton className="">Start your design</MyButton>
-        <MyButton className="">Browse all products</MyButton>
-    </article>
-  )
+    return (
+        <article className='buttons container'>
+            <h2 className="getToIt">Let`s get to it</h2>
+            <div className="btn">
+                <MyButton className="myButton first_btn">Start your design</MyButton>
+                <MyButton className="white-btn second_btn">Browse all products</MyButton>
+            </div>
+        </article>
+    )
 }
 
 export default Buttons
