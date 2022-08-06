7968115deae873ece97a9fc4d1e1ca177b447b37
Meshhur <meshhuratageldiyev010@gmail.com>
2022-08-06 10:07:17 +0300

    finished products

--- src/layout/customs/Customs.jsx
+++ src/layout/customs/Customs.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import "./Customs.css";
+import eq_pic_1 from "../../images/png/card.png";/*импортирую картинки в формате .png, потому что при .jpg у них белый фон */
+import eq_pic_2 from "../../images/png/Rectangle2.png";
+import eq_pic_3 from "../../images/png/equipment.png";
+import MyButton from '../../components/UI/button/MyButton';
+
 
 function Customs() {
   return (
@@ -7,6 +12,34 @@ function Customs() {
         <h2 className="customs-head">
         Fast and easy custom boxes your customers will love
         </h2>
+        <div className="equipments">
+            <article className="equipment">
+                <img src={eq_pic_1} alt="equipment" className="eq-picture" />
+                <h3 className='customs-headline'>You Order</h3>
+                <p className="customs-about">
+                    Design and order boxes in a few clicks.
+                </p>
+            </article>
+
+            <article className="equipment">
+                <img src={eq_pic_2} alt="equipment" className="eq-picture" />
+                <h3 className='customs-headline'>We Proof</h3>
+                <p className="customs-about">
+                    Approve your designs before production.
+                </p>
+            </article>
+
+            <article className="equipment">
+                <img src={eq_pic_3} alt="equipment" className="eq-picture" />
+                <h3 className='customs-headline'>We Print & Ship</h3>
+                <p className="customs-about">
+                    Your order gets printed and shipped with tracking.
+                </p>
+            </article>
+
+        </div>
+
+        <MyButton className="myButton start-btn">Start your design</MyButton>
         
     </section>
   );
